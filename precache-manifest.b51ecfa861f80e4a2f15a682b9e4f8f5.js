c28b5d03e7a34debbe504677f071848dfff7b4ae
Jonelle Samuel <jonelle.samuel92@gmail.com>
2021-12-04 15:06:53 -0500

    Updates

---
+++ precache-manifest.b51ecfa861f80e4a2f15a682b9e4f8f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62361a112ad006a781c686905a336282",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "f650bbb3fe6050e498c2",
+    "url": "/project-4-assessment/static/css/main.72dab6d0.chunk.css"
+  },
+  {
+    "revision": "14811cf29870f0dba4af",
+    "url": "/project-4-assessment/static/js/2.6b24cb24.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/project-4-assessment/static/js/2.6b24cb24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e9e7f7a3baeb4166ae",
+    "url": "/project-4-assessment/static/js/3.194390a6.chunk.js"
+  },
+  {
+    "revision": "f650bbb3fe6050e498c2",
+    "url": "/project-4-assessment/static/js/main.01f5d4d8.chunk.js"
+  },
+  {
+    "revision": "0d93bf0d889ef775143f",
+    "url": "/project-4-assessment/static/js/runtime-main.6e603953.js"
+  }
+]);
